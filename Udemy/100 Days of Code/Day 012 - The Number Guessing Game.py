4c38dbd01b9ae2b80078b91ad40e3776742c9398
Douglas Adones <contato.douglas_ad@outlook.com>
2021-12-25 09:15:18 -0300

    Novos exercícios

--- Udemy/100 Days of Code/Day 012 - The Number Guessing Game.py
+++ Udemy/100 Days of Code/Day 012 - The Number Guessing Game.py
@@ -10,27 +10,57 @@ def adivinha():
    / /\ \    / _` | | | \ \ / / | | | '_ \  | '_ \   / _ \                       
   / ____ \  | (_| | | |  \ V /  | | | | | | | | | | |  __/                       
  /_/    \_\  \__,_| |_|   \_/   |_| |_| |_| |_| |_|  \___|                       
-                                                                                 
-                                                                                 
+
+
                       __                                      _         _____    
                      /_/                                     | |    _  |  __ \   
    ___      _ __    _   _   _ __ ___     ___   _ __    ___   | |   (_) | |  | |  
   / _ \    | '_ \  | | | | | '_ ` _ \   / _ \ | '__|  / _ \  | |       | |  | |  
  | (_) |   | | | | | |_| | | | | | | | |  __/ | |    | (_) | |_|    _  | |__| |  
   \___/    |_| |_|  \__,_| |_| |_| |_|  \___| |_|     \___/  (_)   (_) |_____/   
-                                                                                 
-                                                                                 
+
+
 ''')
     print('Bem vindo ao Jogo de adivinhação de número!\nEstou pensando em um número entre 1 e 100.')
     print(f'Pssst, a resposta correta é {numero}')
-    dificuldade = str(input('Escolha uma dificuldade. Digite "fácil" ou "dificil": ')).strip().lower()
-    if dificuldade in 'facilfácil':
-        chances = 5
-    else:
-        chances = 10
+
+
+    # Escolhendo a dificuldade
+    while True:
+        dificuldade = str(input('Escolha uma dificuldade. Digite "fácil" ou "difícil": ')).strip().lower()
+        if dificuldade == '':
+            print('Escolha uma dificuldade válida')
+        elif dificuldade == 'facil' or dificuldade == 'fácil':
+            chances = 10
+            break
+        elif dificuldade == 'dificil' or dificuldade == 'difícil':
+            chances = 5
+            break
+        else:
+            print('Escolha uma dificuldade válida')
     print(f'Você tem {chances} tentativas para descobrir qual o número!')
+
+
+    # Jogando
     while chances > 0:
-        palpite = int(input('Tente adivinhar: '))
+
+
+        #Pedindo um palpite
+        while True:
+            try:
+                while True:
+                    palpite = int(input('Tente adivinhar: '))
+                    if 0 < palpite <= 100:
+                        break
+                    else:
+                        print('Escolha um número entre 1 e 100!')
+            except:
+                print('Ocorreu um erro! tente novamente.')
+            else:
+                break
+
+
+        #Verificando se a resposta está ou não correta
         if palpite == numero:
             return print(f'Você adivinhou! A resposta é {numero}')
         elif palpite > numero:
